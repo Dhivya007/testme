304683555d941ba12b428821e85c1927e75f017d
Dhivya007 <slsdhivya@gmail.com>
2016-08-18 20:51:06 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,27 @@
+package com.wipro.logical;
+
+import java.util.Scanner;
+
+public class Factorial 
+{
+public static void main(String[] args)
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the number");
+ int num=sc.nextInt();
+ int fact=1;
+ if((num==0) || (num==1))
+ {
+	 System.out.println("Factorial of "+num+" is "+fact);
+ }
+ else
+ {
+	 for(int i=1;i<=num;i++)
+	 {
+		 fact=fact*i;
+	 }
+	 System.out.println("Factorial of "+num+" is "+fact);
+ }
+ sc.close();
+}
+}
